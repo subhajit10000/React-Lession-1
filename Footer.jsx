a0d3122c126ead85aa2352eac855723a0244e233
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-01 19:30:19 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -5,8 +5,7 @@ function Footer() {
             style={{ backgroundColor: "#0b2b5f", color: "white" }}
         >
             
-            <footer className="py-5 container">
-                
+            <footer className="py-5 container">                
                 <div className="row">
                     <div className="col-6 col-md-2 mb-3">
                         <h5>Section</h5>
