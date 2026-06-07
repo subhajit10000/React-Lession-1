ecfbeab24a115fc9eafff532843a822b7f60d648
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-07 20:42:29 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -115,14 +115,16 @@ function Navbar() {
             </ul>
 
             {/* Search Form */}
-            <form className="d-flex" role="search">
+            <form className="d-flex" role="search" onSubmit={(e) => e.preventDefault()}>
               <input
-                className="form-control me-2"
+                className="form-control me-2 bg-dark text-light border-secondary"
                 type="search"
-                placeholder="Search"
+                placeholder="Type to search..."
+                aria-label="Search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
               />
-
-              <button className="btn btn-outline-light" type="submit">
+              <button className="btn btn-outline-info" type="submit">
                 Search
               </button>
             </form>
